b23f68c09bc0777caf4711de19473a099f49ecf5
texasky <texasssky@gmail.com>
2023-09-14 18:03:25 +0200

    Comments fixes

--- activityMonitor.js
+++ activityMonitor.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 const os = require('os');
 
 let fullData = '';
-let dataChunks = 0;
-
+let writingLogInterval;
 const properShellCommand = os.platform() === 'win32'
     ? 'powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + \' \' + $_.CPU + \' \' + $_.WorkingSet }"'
     : 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
 
-const spawnProcess = (command, args) => {
+const spawnProcess = (command, args = []) => {
     const process = childProcess.spawn(command, args, {shell: true});
 
     process.stderr.on('data', (data) => {
@@ -19,20 +18,23 @@ const spawnProcess = (command, args) => {
     process.stdout.on('data', (data) => {
         console.clear();
 
-        if (dataChunks >= 60) {
-            fs.appendFile('activityMonitor.log', fullData, (err) => {
-                if (err) throw err;
-            });
+        if(!writingLogInterval) {
+            writingLogInterval = setInterval(() => {
+                fs.writeFile('activityMonitor.log', fullData, (err) => {
+                    if (err) throw err;
+                });
 
-            fullData = '';
-            dataChunks = 0;
+                fullData = '';
+            }, 60000);
         }
 
         const currentUnixTime = Math.floor(new Date().getTime() / 1000);
-
         fullData += `UNIX Time ${currentUnixTime}: ${data}`;
-        dataChunks += 1;
+
         console.log(`${data}`);
     });
+
+    setTimeout(() => spawnProcess(properShellCommand), 100);
 }
-setInterval(() => spawnProcess(properShellCommand, []), 100)
+
+spawnProcess(properShellCommand, [])
